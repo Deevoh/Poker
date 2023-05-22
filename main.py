25d48075f44f574a58b5f9867421eb9d6b3129b3
Devin Zacharias <devinzacharias@gmail.com>
2023-05-21 22:01:30 -0600

    Update main.py
    Plenty of work done to get things functional.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import random
 from art import logo
 
@@ -16,10 +17,47 @@ def deal_hand(deck, num_cards):
     return hand
 
 def display_hand(hand, balance, bet):
-    print(f"\nBank: ${balance}  |  Bet: ${bet}")
+    print(f"\nBank: ${int(balance)}  |  Bet: ${int(bet)}")
     print("Your hand:")
     for card in hand:
-        print(card, end=" ")
+        print("┌─────┐", end=" ")
+    print()
+    for card in hand:
+        rank = card[:-1]
+        print(f"| {rank:<2}  |", end=" ")
+    print()
+    for card in hand:
+        print("|     |", end=" ")
+    print()
+    for card in hand:
+        suit = card[-1]
+        print(f"|  {suit}  |", end=" ")
+    print()
+    for card in hand:
+        print("└─────┘", end=" ")
+    print()
+    for i, card in enumerate(hand):
+        print(f"  [{i+1}]", end="   ")
+    print()
+
+def display_hand_post_hold(hand):
+    print("\nYour new hand:")
+    for card in hand:
+        print("┌─────┐", end=" ")
+    print()
+    for card in hand:
+        rank = card[:-1]
+        print(f"| {rank:<2}  |", end=" ")
+    print()
+    for card in hand:
+        print("|     |", end=" ")
+    print()
+    for card in hand:
+        suit = card[-1]
+        print(f"|  {suit}  |", end=" ")
+    print()
+    for card in hand:
+        print("└─────┘", end=" ")
     print()
 
 def select_cards():
@@ -77,10 +115,17 @@ def calculate_score(hand):
             return 2
     return 0
 
-def play_round(balance):
+def play_round(balance, game_over):
+    # if balance <= 0:
+    #     print("Game Over! Your balance is zero.")
+    #     return
     while True:
-        print(f"Bank: ${balance}")
-        bet = int(input("Enter your bet amount: $"))
+        print(f"\nBank: ${int(balance)}")
+        bet = input("Enter your bet amount: $")
+        if not bet.isdigit():
+            print("Please input numbers only.")
+            continue
+        bet = int(bet)
         if bet > balance:
             print("Insufficient balance!")
             continue
@@ -88,62 +133,95 @@ def play_round(balance):
 
     deck = create_deck()
     hand = deal_hand(deck, 5)
+    os.system('cls' if os.name == 'nt' else 'clear')
     display_hand(hand, balance, bet)
 
-    choice = input("\nDo you want to hold any cards? (y/n): ")
-    if choice.lower() == 'n':
-        deck = create_deck()
-        hand = deal_hand(deck, 5)
-    elif choice.lower() == 'y':
-        positions = select_cards()
-        redraw_cards(deck, hand, positions)
+    while True:
+        hold_input = input("\nDo you want to hold any cards? (y/n): ")
+        if hold_input.lower() == 'n':
+            deck = create_deck()
+            hand = deal_hand(deck, 5)
+            break
+        elif hold_input.lower() == 'y':
+            positions = select_cards()
+            redraw_cards(deck, hand, positions)
+            break
+        else:
+            continue
 
-    display_hand(hand, balance, bet)
+    display_hand_post_hold(hand)
     score = calculate_score(hand)
-    print(f"\nBet: ${bet}")
 
     payout_ratio = 0
     if score == 100:  # Royal flush
         payout_ratio = 100
+        print("A royal flush!!")
     elif score >= 80:  # Straight flush
         payout_ratio = 50
+        print("Straight flush!")
     elif score >= 60:  # Four of a kind
         payout_ratio = 20
+        print("Four of a kind!")
     elif score >= 40:  # Full house
         payout_ratio = 10
+        print("Full house!")
     elif score >= 30:  # Flush
         payout_ratio = 5
+        print("Flush!")
     elif score >= 20:  # Straight
         payout_ratio = 3
+        print("Straight!")
     elif score >= 10:  # Three of a kind
         payout_ratio = 2
+        print("Three of a kind!")
     elif score >= 5:  # Two pair
         payout_ratio = 1.5
+        print("Two pair!")
     elif score >= 2:  # Pair
         payout_ratio = 1
+        print("A pair!")
     else:
         payout_ratio = 0
+        print("Nothing..")
 
     payout_amount = bet * payout_ratio
-    print(f"Payout: ${payout_amount}")
+    print(f"\nBet: ${int(bet)}")
+    print(f"Payout: ${int(payout_amount)}")
     balance -= bet
     balance += payout_amount
-    print(f"Balance: ${balance}")
+    print(f"Balance: ${int(balance)}")
 
-    while True:
-        new_round_input = input("\nPlay again? (y/n) : ")
+    if balance <= 0:
+        print("\nGame Over! Your balance is zero.")
+        game_over = True
+
+    while not game_over:
+        new_round_input = input("\nDeal another hand? (y/n): ")
         if new_round_input.lower() == 'n':
-            print("Goodbye.\n")
-            exit()
+            print(f"\nYou cashed out with a total of ${int(balance)}.")
+            game_over = True
         elif new_round_input.lower() == 'y':
-            play_round(balance)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            play_round(balance, game_over)
+        else:
+            continue
+
+    while game_over:
+        new_game_input = input("\nStart a new game? (y/n): ")
+        if new_game_input.lower() == 'n':
+            print("\nGoodbye.\n")
+            exit()
+        elif new_game_input.lower() == 'y':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            play_game()
         else:
             continue
 
 def play_game():
-    balance = 200
+    balance = 400
+    game_over = False
     print(logo)
-    play_round(balance)
+    play_round(balance, game_over)
 
 
 play_game()
